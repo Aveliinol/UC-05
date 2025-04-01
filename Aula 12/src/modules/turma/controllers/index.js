9443b084d7ec1de3843af1cd7f79d9b023bfca33
Lucas <lugabriell084@gmail.com>
2025-04-01 13:42:31 -0300

    feat: refatorei o controlador e modelo de turma para métodos mais descritivos e tratamento de erros

--- Aula 12/src/modules/turma/controllers/index.js
+++ Aula 12/src/modules/turma/controllers/index.js
@@ -1,22 +1,50 @@
 const TurmaModel = require("../models/index")
 
 class TurmaController{
-    static async criar(nome, modalidade, dataInicio, dataTermino){
+    static async criarTurma(req, res){
+        try {
+            const {nome, modalidade, dataInicio, dataTermino} = req.body
+            if(!nome || !modalidade || !dataInicio || !dataTermino){
+                return res.status(404).json({msg:"Todos os campos devem ser preenchidos"})
+            }
+            const turma = await TurmaModel.criarTurma(nome, modalidade, dataInicio, dataTermino)
+            res.status(201).json({msg:"Turma criada com sucesso", turmas: turma})
+        } catch (error) {
+            res.status(500).json({msg: "Erro interno do servidor. Por favor tente mais tarde!", erro: error.message})
+        }
+    }
+    static async editarTurma(req, res){
+        try {
+            const id = req.params.id
+            const {nome, modalidade, dataInico, dataTermino} = req.body
+            if(!id){
+                return res.status(404).json({msg:"Informe o ID"})
+            }
+            if(!nome || !modalidade || !dataInicio || !dataTermino){
+                return res.status(404).json({msg:"Todos os campos devem ser preenchidos"})
+            }
+            const editTurma = await TurmaModel.editarTurma(id, nome, modalidade, dataInico, dataTermino)
+            if(editTurma.length === 0){
+                return res.status(404).json({msg:'Turma não encontrada'})
+            }
+            res.status(200).json({msg:"Turma editada com sucesso", turma: editTurma})
+            
+        } catch (error) {
+            res.status(500).json({msg: "Erro interno do servidor. Por favor tente mais tarde!", erro: error.message})
+        }
+    }
+    static async listarTurmas(){
 
     }
-    static async editar(){
+    static async listaTurmaPorID(){
 
     }
-    static async listarAll(){
+    static async deletarTurmas(){
 
     }
-    static async listaPorID(){
-
-    }
-    static async deletarAll(){
-
-    }
-    static async deletarPorID(){
+    static async deletarTurmaPorID(){
 
     }
 }
+
+module.exports = TurmaController
